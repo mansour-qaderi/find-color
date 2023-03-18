67bb87a6a8f84b3de5e5fcc98fe15723c7f1a04c
mansour-qaderi <mansourqaderi21@gmail.com>
2023-03-18 19:38:01 +0430

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "./styles/app.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <main className="body">
+        <h2>Hello World!</h2>
+      </main>
+    </div>
+  );
+};
+export default App;
